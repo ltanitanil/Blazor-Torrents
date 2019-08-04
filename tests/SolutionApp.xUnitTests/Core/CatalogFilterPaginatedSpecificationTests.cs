4073240c07bce0f8d5ecb76fbfaf70ab68a5f742
Tatsiana Panasiuk <Tatsiana_Panasiuk@epam.com>
2019-08-04 16:43:25 +0300

    Added new tests for CatalogFilterPaginatedSpecification and CatalogFilterSpecification.

---
+++ tests/SolutionApp.xUnitTests/Core/CatalogFilterPaginatedSpecificationTests.cs
@@ -0,0 +1,30 @@
+﻿using Blazor.Core.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SolutionApp.xUnitTests.Core
+{
+    public class CatalogFilterPaginatedSpecificationTests
+    {
+        public static TheoryData<int, int, string, int?, long?, long?, DateTimeOffset?, DateTimeOffset?> Data =>
+            new TheoryData<int, int, string, int?, long?, long?, DateTimeOffset?, DateTimeOffset?>
+            {
+                { 0, 0, null, null, null, null, null, null},
+                { 5, 5, string.Empty, 5, long.MinValue, long.MaxValue, DateTimeOffset.MinValue, DateTimeOffset.MaxValue}
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ReturnValidCatalogFilterPaginatedSpecification(int skip, int take, string search, int? forumId, long? sizeFrom, long? sizeTo, DateTimeOffset? dateFrom, DateTimeOffset? dateTo)
+        {
+            var result = new CatalogFilterPaginatedSpecification(skip, take, search, forumId, sizeFrom, sizeTo, dateFrom, dateTo);
+
+            Assert.NotNull(result.Criteria);
+            Assert.True(result.IsPagingEnabled);
+            Assert.True(result.Skip == skip, $"Expected skip={skip} doesn't match the actual skip={result.Skip}");
+            Assert.True(result.Take == take, $"Expected take={take} doesn't match the actual take={result.Take}");
+        }
+    }
+}
---
+++ tests/SolutionApp.xUnitTests/Core/CatalogFilterSpecificationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blazor.Core.Specifications;
+using Xunit;
+
+namespace SolutionApp.xUnitTests.Core
+{
+    public class CatalogFilterSpecificationTests
+    {
+        public static TheoryData<string, int?, long?, long?, DateTimeOffset?, DateTimeOffset?> Data =>
+            new TheoryData<string, int?, long?, long?, DateTimeOffset?, DateTimeOffset?>
+            {
+                { null, null, null, null, null, null},
+                { string.Empty, 5, long.MinValue, long.MaxValue, DateTimeOffset.MinValue, DateTimeOffset.MaxValue}
+            };
+
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void ReturnValidCatalogFilterPaginatedSpecification(string search, int? forumId, long? sizeFrom, long? sizeTo, DateTimeOffset? dateFrom, DateTimeOffset? dateTo)
+        {
+            var result = new CatalogFilterSpecification(search, forumId, sizeFrom, sizeTo, dateFrom, dateTo);
+
+            Assert.NotNull(result.Criteria);
+            Assert.False(result.IsPagingEnabled);
+        }
+    }
+}
