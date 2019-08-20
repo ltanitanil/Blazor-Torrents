cd32869cbec44f989aadc33ff7481c8df2d3ffef
ltanitanil <41678936+ltanitanil@users.noreply.github.com>
2019-08-20 16:25:40 +0300

    Refactoring (#22)
    * changed the pagination model.
    
    * Deleted old tests. Added a new TorrentsServiceTests. Fixed broken tests

--- src/Blazor.Server.BusinessLayer/Services/TorrentsService/TorrentsService.cs
+++ src/Blazor.Server.BusinessLayer/Services/TorrentsService/TorrentsService.cs
@@ -24,6 +24,9 @@ namespace Blazor.Server.BusinessLayer.Services.TorrentsService
 
         public async Task<(IReadOnlyList<Forum>, long)> GetDataToFilter(int forumsCount)
         {
+            if (forumsCount < 0)
+                throw new AppException(ExceptionEvent.InvalidParameters, "forumsCount can't be negative");
+
             return (await _torrentsRepository.GetPopularForumsAsync(forumsCount),
                 await _torrentsRepository.MaxAsync(x => x.Size));
         }
@@ -45,11 +48,13 @@ namespace Blazor.Server.BusinessLayer.Services.TorrentsService
             return torrent;
         }
 
-        public async Task<(IReadOnlyList<Torrent>, int)> GetTorrentsAndCount(int pageIndex, int itemsPage, string search,
+        public async Task<(IReadOnlyList<Torrent>, int)> GetTorrentsAndCount(int pageIndex, int itemsPerPageCount, string search,
             int? forumId, long? sizeFrom, long? sizeTo, DateTimeOffset? dateFrom, DateTimeOffset? dateTo)
         {
             if (pageIndex < 0)
                 throw new AppException(ExceptionEvent.InvalidParameters, "Page can't be negative");
+            if (itemsPerPageCount < 0)
+                throw new AppException(ExceptionEvent.InvalidParameters, "Number of elements per page can't be negative");
 
             Expression<Func<Torrent, bool>> filter = x => (string.IsNullOrEmpty(search) || x.Title.Contains(search))
                                                           && (!forumId.HasValue || x.ForumId == forumId)
@@ -59,9 +64,12 @@ namespace Blazor.Server.BusinessLayer.Services.TorrentsService
                                                           && (!dateTo.HasValue || x.RegisteredAt <= dateTo);
 
             var torrents = await _torrentsRepository.GetAll(filter)
-                               .Skip(pageIndex * itemsPage)
-                               .Take(itemsPage)
-                               .ToListAsync() ?? throw new AppException(ExceptionEvent.NotFound, $"Torrents not found");
+                .Skip(pageIndex * itemsPerPageCount)
+                .Take(itemsPerPageCount)
+                .ToListAsync();
+
+            if (!torrents.Any())
+                throw new AppException(ExceptionEvent.NotFound, "Torrents not found");
 
             var count = await _torrentsRepository.GetAll(filter).CountAsync();
 
