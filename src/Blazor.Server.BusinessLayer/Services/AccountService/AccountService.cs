fd45ee53f9901c7145588c8196291c4559d69412
Tatsiana Panasiuk <Tatsiana_Panasiuk@epam.com>
2019-09-03 18:38:14 +0300

    Deleted unnecessary namespace

--- src/Blazor.Server.BusinessLayer/Services/AccountService/AccountService.cs
+++ src/Blazor.Server.BusinessLayer/Services/AccountService/AccountService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AutoMapper;
 using Blazor.Server.BusinessLayer.Entities;
 using Blazor.Server.BusinessLayer.Exceptions;
 using Blazor.Server.BusinessLayer.Services.JwtTokenService;
